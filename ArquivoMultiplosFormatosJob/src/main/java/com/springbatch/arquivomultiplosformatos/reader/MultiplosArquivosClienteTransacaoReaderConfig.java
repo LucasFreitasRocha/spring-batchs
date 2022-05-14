582c533fb9227997ecc036b3aaf9b5b0816dca54
LucasFreitasRocha <rochadefreitaslucas@gmail.com>
2022-05-14 17:45:57 -0300

    finish read multiple files

--- ArquivoMultiplosFormatosJob/src/main/java/com/springbatch/arquivomultiplosformatos/reader/MultiplosArquivosClienteTransacaoReaderConfig.java
+++ ArquivoMultiplosFormatosJob/src/main/java/com/springbatch/arquivomultiplosformatos/reader/MultiplosArquivosClienteTransacaoReaderConfig.java
@@ -1,4 +1,28 @@
 package com.springbatch.arquivomultiplosformatos.reader;
 
+
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.MultiResourceItemReader;
+import org.springframework.batch.item.file.builder.MultiResourceItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+@Configuration
 public class MultiplosArquivosClienteTransacaoReaderConfig {
+
+    @StepScope
+    @Bean
+    public MultiResourceItemReader multiplosArquivosClienteTransacaoReader(
+            @Value("#{jobParameters['arquivoClientes']}") Resource[] arquivoCliente,
+            FlatFileItemReader leituraArquivoMultiplosFormatosReader
+    ){
+        return new MultiResourceItemReaderBuilder<>()
+                .name("multiplosArquivosClienteTransacaoReader")
+                .resources(arquivoCliente)
+                .delegate(new ArquivoClienteTransacaoReader(leituraArquivoMultiplosFormatosReader))
+                .build();
+    }
 }
