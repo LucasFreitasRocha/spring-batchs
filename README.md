b3fe934da4b0e974495d254d008d4cfafd0275b4
LucasFreitasRocha <rochadefreitaslucas@gmail.com>
2022-05-21 17:58:29 -0300

    fixed-width file writing

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ a study of spring batchs
 
 
 arquivoClientes=file:files/clientes.txt
+
+arquivoSaidaClientes=file:files/arquivoSaidaClientes.txt
---
+++ writrers/ArquivoLarguraFixaJob/src/main/java/com/springbatch/arquivolargurafixa/writer/ArquivoLarguraFixaWriterConfig.java
@@ -0,0 +1,31 @@
+package com.springbatch.arquivolargurafixa.writer;
+
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.springbatch.arquivolargurafixa.dominio.Cliente;
+import org.springframework.core.io.Resource;
+
+@Configuration
+public class ArquivoLarguraFixaWriterConfig {
+	// FIXME: Modificar para escrever em arquivo de largura fixa!
+
+	@StepScope
+	@Bean
+	public FlatFileItemWriter<Cliente> escritaArquivoLarguraFixaWriter(
+			@Value("#{jobParameters['arquivoSaidaClientes']}") Resource arquivoSaidaClientes
+	) {
+		return new FlatFileItemWriterBuilder<Cliente>()
+				.name("escritaArquivoLarguraFixaWriter")
+				.resource(arquivoSaidaClientes)
+				.formatted()
+				.format("%-9s %-9s %-2s %-19s")
+				.names("nome", "sobrenome", "idade", "email")
+				.build();
+	}
+}
